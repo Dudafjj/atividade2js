61be1ee99adce99db56fc718eb3f0baa8a2e4ade
Dudafjj <102737580+Dudafjj@users.noreply.github.com>
2022-05-05 10:51:55 -0300

    Add files via upload

---
+++ at2.js
@@ -0,0 +1,30 @@
+//1
+function nome (linda){
+    console.log(linda)
+    return
+}
+
+nome('Eduarda Ferreira')
+
+//2
+function soma (a,b){
+    return console.log(a+b)
+}
+
+soma(7,5)
+
+//3
+function multi (a,b){
+    return console.log(a*b)
+}
+
+multi(9,9)
+
+//4
+function med (a,b,c,d){
+    let res = (a+b+c+d)/4
+    console.log(res) 
+    return 
+}
+
+med(9,8,4,3)
